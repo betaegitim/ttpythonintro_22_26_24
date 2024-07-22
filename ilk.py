d2408def36221f3f3a5d62bcf1a3e81f22240c69
betaegitim <info@betaegitim.com>
2024-07-22 06:48:06 +0000

    22072024_1

--- ilk.py
+++ ilk.py
@@ -1,1 +1,10 @@
-print("Merhaba")
+liste = ["ahmet","emrullah","ervanur","fatih","haydar","mustafa","nurican","ozgur","rabia","zeynep","cevaplar"]
+import os
+filename = "ilk.ipynb"
+for item in liste:
+    filename = f"{item}_" + filename
+    if not os.path.exists(f"Egzersizler/{item}/"):
+        os.mkdir(f"Egzersizler/{item}/")
+    else:
+        open(f"Egzersizler/{item}/{filename}","w+")
+        print("Zaten Oluşturuldu.")
