84723d39ff07e882981175f7b4107d8aa707e6d7
betaegitim <info@betaegitim.com>
2024-07-24 09:01:01 +0000

    2407_5

---
+++ Egzersizler/haydar/egzersiz_donguler.py
@@ -0,0 +1,13 @@
+""" 
+ 
+    "acilar = [(90, 84), (45, 45), (60, 60), (53, 89), (60, 116), (30, 60), (71, 46), (23, 64)]\n",
+    "yukarıda verilen açı listesini kullanarak üçgenlerin aşağıda verilen seçenekler biri olmak üzere tipini ekrana yazdıran python kodunu yazalım\n",
+    "* ikizkenar üçgen\n",
+    "* dik üçgen\n",
+    "* eşkenar üçgen\n",
+    "* çeşitkenar üçgen\n",
+    
+
+"""
+
+print(2)
--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # ttpythonintro_22_26_24
+
+Eğitim sırasında paylaşılan linkler
+* https://www.geeksforgeeks.org/getting-started-with-python-programming/?ref=shm
