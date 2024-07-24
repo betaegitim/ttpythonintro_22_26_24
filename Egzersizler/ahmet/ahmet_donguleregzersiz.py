0ab96f0bfba0a3d316a03986737a827f54cc46ee
betaegitim <info@betaegitim.com>
2024-07-24 11:48:37 +0000

    2407_4

--- Egzersizler/ahmet/ahmet_donguleregzersiz.py
+++ Egzersizler/ahmet/ahmet_donguleregzersiz.py
@@ -7,4 +7,11 @@
     "1. Armstrong Sayısı (371 => basamak sayısı 3 olduğu için `3**3 + 7**3 + 1**3 = 371` olduğu için 371 armstrong sayısıdır.)\n",
     "2. 10000 sayısına kadar olan armstrong sayılarını ekrana yazdıran python kodunu yazınız"
    
-   input=
+
+sayi=int(input("Bir sayı giriniz:"))
+if sayi<=10:
+print("10'dan büyük bir sayı giriniz!")
+else:
+    for (int i=1;sayi;i++):
+        for int(j=2;i;j++):
+            if i % j == 0:
--- ilk.py
+++ ilk.py
@@ -3,13 +3,13 @@ liste = ["ahmet","emrullah","ervanur","fatih",
 "mustafa","nurican","ozgur","rabia","zeynep","cevaplar","halilyasin"]
 import os
 import shutil
-konu = "02_02_donguleregzersiz"
+konu = "03_Fonksiyonlar_01"
 if konu.find("egzersiz") > 0:
     egzersizliste = liste.copy()
     egzersizliste.remove("cevaplar")
     for item in egzersizliste:
         filename = ""
-        filename = f"02_{item}_{konu}.ipynb"
+        filename = f"{item}_{konu}.ipynb"
         kaynak = f"Egzersizler/cevaplar/{konu}.ipynb"
         hedef = f"Egzersizler/{item}/{filename}"
         # os.remove(hedef)
